d98294f0867704d66a73d6b08193557fa560e193
JamonNage27 <nagelygansaenjamon@gmail.com>
2024-12-20 06:34:24 +0800

    array activity.js

---
+++ array activity.js
@@ -0,0 +1,13 @@
+//array activity.js
+let favSingers = ["Taylor Swift", "Nateman", "Hevabi"];
+
+console.log(favSingers[1]); 
+
+let favNumbers = [7, 13, 21, 37];
+
+let mixedArr = ["string", ["otherarray"], 123, true];
+
+console.log(mixedArr[0]);
+console.log(mixedArr[1]); 
+console.log(mixedArr[2]); 
+console.log(mixedArr[3]); 
